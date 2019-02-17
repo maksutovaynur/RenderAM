787c03fa12614e224250d58d644d9ba80ef68caa
Aynur Maksutov <maksutov_aynur@mail.ru>
2019-02-17 19:02:29 +0300

    Add files via upload

---
+++ build/CMakeFiles/outfile.bin.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/outfile.bin.dir/src/Main.cpp.o"
+  "../bin/outfile.bin.pdb"
+  "../bin/outfile.bin"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/outfile.bin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
